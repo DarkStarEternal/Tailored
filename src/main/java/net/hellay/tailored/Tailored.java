0ea7fb427376b5727c962e95049fe17de4b79715
BuildTools <unconfigured@null.spigotmc.org>
2025-10-30 17:25:32 +0100

    Initial Commit

---
+++ src/main/java/net/hellay/tailored/Tailored.java
@@ -0,0 +1,10 @@
+package net.hellay.tailored;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tailored implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/hellay/tailored/client/TailoredClient.java
@@ -0,0 +1,10 @@
+package net.hellay.tailored.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TailoredClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
